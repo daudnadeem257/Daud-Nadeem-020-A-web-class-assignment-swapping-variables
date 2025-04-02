70c94774515aff5026498711a9d443dde9e915b1
Daud Nadeem <daudnadeem257@gmail.com>
2025-04-02 20:09:42 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a = "3";
+var b = "8";
+
+console.log("Before swap a is " + a);
+console.log("Before swap b is " + b);
+
+var c = a;
+a = b;
+b = c;
+
+console.log("After swap a is " + a);
+console.log("After swap b is " + b);
